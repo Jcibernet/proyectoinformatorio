079e9db650b9e77b3082ca8b5d81f2543a1ddbcc
Alejandro Chicala <chicalaale6@gmail.com>
2021-11-30 15:24:54 -0300

    base.py modificado

--- ecoblog/ecoblog/settings/base.py
+++ ecoblog/ecoblog/settings/base.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'ecoblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(BASE_DIR),'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
