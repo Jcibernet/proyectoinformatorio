bf6f2de5ccc0e77ce963b4e6c3eb5c29337e2642
Alejandro Chicala <chicalaale6@gmail.com>
2021-12-18 17:05:48 -0300

    m

--- ecoblog/apps/posts/views.py
+++ ecoblog/apps/posts/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse_lazy
 from apps.usuarios.models import Usuario
 from .forms import Formulario_nuevo_post
 
+
 import random
 
 from .models import Comentario, Post, Categoria
@@ -61,7 +62,8 @@ def DetallePost(request, pk):
     ctx['masPosts'] = topTres
     ctx['allOds'] = ods
     ctx['comentarios'] = comentarios
-    ctx['url'] = f"https://web.whatsapp.com/send?text=http%3A%2F%2F127.0.0.1:8000%2Fposteos%2F{post.pk}"
+    ctx['url'] = request
+    # ctx['url'] = f"https://web.whatsapp.com/send?text=http%3A%2F%2F{request.get_host}%2Fposteos%2F{post.pk}"
 
     return render(request, 'posteos/detallePosteo.html', ctx)
 
