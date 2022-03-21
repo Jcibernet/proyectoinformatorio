b44d7c68b30828c180a57d5508289f5000e88e9c
juan <campiasjuan@gmail.com>
2022-03-21 19:17:33 -0300

    Complete refactor

--- ecoblog/wsgi.py
+++ ecoblog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecoblog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecoblog.settings.production')
 
 application = get_wsgi_application()
